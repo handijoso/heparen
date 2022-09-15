35d664bcb811ef37479d7cf750a172adf8ac9821
Ignatius Handijoso S <108178271+handijoso@users.noreply.github.com>
2022-09-15 11:02:54 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+//Showing content/element while scrolling
+let windowHeight = window.innerHeight;
+
+const reveal = () => {
+    let reveals = document.querySelectorAll(".reveal");
+
+    for (let i = 0; i < reveals.length; i++) {
+        let revealTop = reveals[i].getBoundingClientRect().top;
+        let revealPoint = 50;
+
+        if (revealTop < windowHeight - revealPoint) {
+            reveals[i].classList.add("active");
+            console.log("active");
+        }
+        else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+window.addEventListener("scroll", reveal);
+
+(function () {
+
+    'use strict';
+
+    // define variables
+    var items = document.querySelectorAll(".timeline li");
+
+    // check if an element is in viewport
+    // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+    function isElementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    function callbackFunc() {
+        for (var i = 0; i < items.length; i++) {
+            if (isElementInViewport(items[i])) {
+                items[i].classList.add("in-view");
+            }
+        }
+    }
+
+    // listen for events
+    window.addEventListener("load", callbackFunc);
+    window.addEventListener("resize", callbackFunc);
+    window.addEventListener("scroll", callbackFunc);
+
+})();
